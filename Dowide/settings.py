b8d3fa3a57aeb8e059808610bd45c5c9230046d1
kidlinux7 <princeherman08@gmail.com>
2022-08-10 10:04:41 +0300

    Sixth commit

--- Dowide/settings.py
+++ Dowide/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-959^t*koyfpx=w_ubzvvo356es*n7jdto9yyg!s1ts%of3x4%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
